95d4aad6afef68b03227fbaa51673659cad24641
vara0715 <88980746+vara0715@users.noreply.github.com>
2022-12-15 11:23:33 +0530

    Update ds-assignment-5-t5.py

--- ds-assignment-5-t5.py
+++ ds-assignment-5-t5.py
@@ -1,32 +1,31 @@
-class Account: 
-    def _init_(self, title=None, balance=0):
+class Account:
+    def __init__(self, title=None, balance=0):
         self.title = title
         self.balance = balance
     
     def withdrawal(self, amount):
-        self.balance = self.balance - amount
-    
+        self.amount = amount
+        self.balance = self.balance - self.amount
 
     def deposit(self, amount):
-        self.balance = self.balance + amount
-    
+        self.amount = amount
+        self.balance = self.balance + self.amount
     def getBalance(self):
         return self.balance
 
-
 class SavingsAccount(Account):
-    def _init_(self, title=None, balance=0, interestRate=0):
-        super()._init_(title, balance)
-        self.interestRate = interestRate
+    def __init__(self, title=None, balance=0, interestRate=0):
+            super().__init__(title, balance)
+            self.interestRate = interestRate
     
     def interestAmount(self):
-        return (self.balance * self.interestRate / 100)
+        return (self.balance * self.interestRate / 100 ) 
+        
 
-
-obj1 = SavingsAccount("preethi", 2000, 5)
-print("Initial Balance:", obj1.getBalance())
-obj1.withdrawal(500)
-print("Balance after withdrawal:", obj1.getBalance())
-obj1.deposit(500)
-print("Balance after deposit:", obj1.getBalance())
-print("Interest on current balance:", obj1.interestAmount())
+obj = SavingsAccount("Pratham",1000,5)
+print("Initial Balance : ",obj.getBalance())
+obj.withdrawal(500)
+print("Balance after withdrawal : ",obj.getBalance())
+obj.deposit(1500)
+print("After deposit : ",obj.getBalance())
+print("Intrest on current balance : ",obj.interestAmount())
